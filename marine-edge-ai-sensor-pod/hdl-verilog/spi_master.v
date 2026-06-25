76f6556623fbd42b2f433c9e110a6d5a35ddc15f
kalebwu41 <kwu.1600@gmail.com>
2026-06-25 16:37:23 -0500

    added FSM states

--- marine-edge-ai-sensor-pod/hdl-verilog/spi_master.v
+++ marine-edge-ai-sensor-pod/hdl-verilog/spi_master.v
@@ -1,4 +1,4 @@
-// SPI: FROM AXL345
+// SPI: FROM AXL345(accelerometer) 
 module spi_master (
     input clk, //internal heartbeat
     input rst_n, //active low reset
@@ -7,7 +7,7 @@ module spi_master (
     input [6:0] addr,//address book
     output reg sclk, //clock signal for accelermeter
     output reg MOSI, //master out slave : in FPGA to accelermeter
-    output reg cs_n,
+    output reg cs_n, //accelermeter pays attention when this is low
     output reg [7:0] data_out, //data from accelermeter
     output reg data_valid
 );
@@ -36,6 +36,42 @@ module spi_master (
         end else begin
             state <= next_state;
             //finite machine state starts here.
+            case(next_state)
+                IDLE: begin
+                    cs_n <= 1'b1;
+                    sclk <= 1'b0;
+                    if (start) begin
+                        next_state <= START;
+                    end
+                end
+
+                START: begin
+                    cs_n <= 1'b0;
+                    bit_count <= 4'd0;
+                    //shift reg <=...
+                end
+                TRANSFER: begin
+                    sclk <= ~sclk;//toggel clock
+                    if (sclk == 1'b0) begin
+                        MOSI <= shift_reg[15];
+                        shift_reg <= shift_reg << 1;
+                        bit_count <= bit_count + 1'b1;
+                        
+                    end
+                    if (bit_count == 4'd15 && sclk == 1'b1) begin
+                        next_state <= FINISH;
+                    end
+                end
+                FINISH: begin
+                    cs_n <= 1'b1;
+                    data_valid <= 1'b1;
+                    state <= IDLE;
+                end
+
+                default: begin
+                    next_state <= IDLE;
+                end 
+            endcase
         end
     end
 endmodule
